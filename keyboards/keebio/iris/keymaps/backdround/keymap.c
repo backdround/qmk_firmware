9318c368b1254089b67d5c006e267e73980595d0
Vlad Chepaykyn <backdround@yandex.ru>
2021-04-20 21:26:53 +0300

    remove qwerty layout

--- keyboards/keebio/iris/keymaps/backdround/keymap.c
+++ keyboards/keebio/iris/keymaps/backdround/keymap.c
@@ -9,7 +9,6 @@
 #define SH_TAB RSFT(KC_TAB)
 
 // OS language switch
-#define TO_US KC_CAPS
 #define TO_DV KC_CAPS
 #define TO_RU LSFT(KC_CAPS)
 
@@ -18,19 +17,11 @@
 #define F_PSCR LCTL(KC_PSCR) // Full virtual space
 #define W_PSCR LSFT(KC_PSCR) // Window
 
-// language switch
-enum custom_keycodes {
-    K_US = SAFE_RANGE,
-    K_DV,
-    K_RU
-};
-
 ////////////////////////////////////////
 // Layouts.
 
 enum LAYOUTS {
     _DVORAK,
-    _QWERTY,
     _SYMBOLS,
     _ADDITIONAL,
     _SPECIAL,
@@ -48,32 +39,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_DVORAK] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     _______, KC_1,    K_US,    K_DV,    K_RU,    CUST_,                              CUST_,   KC_VOLU, KC_MUTE, KC_VOLD, _______, _______,
+     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               CUST_,   KC_VOLU, KC_MUTE, KC_VOLD, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      SPEC_,   KC_W,    KC_V,    KC_S,    KC_P,    KC_Y,                               KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    SPEC_,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LCTL, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                               KC_D,    KC_H,    KC_T,    KC_N,    ADD_,    KC_LCTL,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, KC_Z,    KC_Q,    KC_J,    KC_K,    KC_X,    KC_SPC,           SPEC_,   KC_B,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+     KC_LSFT, KC_Z,    KC_Q,    KC_J,    KC_K,    KC_X,    TO_DV,            TO_RU,   KC_B,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     KC_LALT, KC_LGUI, SYMB_,                     SYMB_,   KC_SPC,  KC_LALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
-
-  [_QWERTY] = LAYOUT(
-  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     _______, KC_1,    K_US,    K_DV,    K_RU,    CUST_,                              CUST_,   KC_VOLU, KC_MUTE, KC_VOLD, _______, _______,
-  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     SPEC_,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    SPEC_,
-  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    ADD_,    KC_LCTL,
-  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC,           SPEC_,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LALT, KC_LGUI, SYMB_,                     SYMB_,   KC_SPC,  KC_LALT
-                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
-  ),
-
 
   [_SYMBOLS] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
@@ -119,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_CUSTOMIZATION] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     RGB_SAI, RGB_M_P, BL_BRTG, _______, RESET,   _______,                            _______, RESET,   _______, RGB_SAI, RGB_M_P, BL_BRTG,
+     RGB_SAI, RGB_M_P, BL_BRTG, _______, _______, _______,                            _______, RESET,   _______, RGB_SAI, RGB_M_P, BL_BRTG,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      RGB_SAD, RGB_TOG, BL_TOGG, _______, _______, _______,                            _______, _______, _______, RGB_SAD, RGB_TOG, BL_TOGG,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -136,24 +112,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
     if (!record->event.pressed) {
         return true;
-    }
-
-    switch (keycode) {
-        case K_US:
-            register_code(TO_US);
-            layer_off(_DVORAK);
-            layer_on(_QWERTY);
-            break;
-        case K_DV:
-            register_code(TO_DV);
-            layer_off(_QWERTY);
-            layer_on(_DVORAK);
-            break;
-        case K_RU:
-            register_code16(TO_RU);
-            layer_off(_QWERTY);
-            layer_on(_DVORAK);
-            break;
     }
 
     return true;
